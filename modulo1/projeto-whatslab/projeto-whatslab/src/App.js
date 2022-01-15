78987e8918fc40a8b435fd7a5ca1a53a41296465
paulolopest <paulotarso-lopes@hotmail.com>
2022-01-14 23:52:13 -0300

    So para entregar no formulario

---
+++ modulo1/projeto-whatslab/projeto-whatslab/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+
+      <div className='whatslab'>
+        <header></header>
+        <nav></nav>
+        <main></main>
+        <footer></footer>
+      </div>
+
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
