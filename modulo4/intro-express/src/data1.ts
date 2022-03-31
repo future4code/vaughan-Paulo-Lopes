4685135f63fbc57c6ad32a1e505c145f647a75a2
paulolopest <paulotarso-lopes@hotmail.com>
2022-03-30 23:42:42 -0300

    Exercicio Fluxo de dados

---
+++ modulo4/intro-express/src/data1.ts
@@ -0,0 +1,39 @@
+export const users = [
+    {
+        id: "paulo-tarso-vaughan",
+        name: "Sertanejo",
+        playlist: [
+            { 
+               id: "SAOJKIDJSÇOFJSÇO",
+               name: "Eu beijo",
+               artist: "Roberto Maia",
+               url: "youtube.com/paulolopes"
+            },
+            { 
+                id: "SDIKHFBSDFNSDUIFHJ",
+                name: "Eu Jogo",
+                artist: "Faustao",
+                url: "youtube.com/paulolopes"
+             }
+        ]
+    },
+
+    {
+        id: "yasmin-conc-shaw",
+        name: "Modão",
+        playlist: [
+            { 
+               id: "SAOJKIDJSÇOFJSÇO",
+               name: "Eu destrato",
+               artist: "Carlinhos Maia",
+               url: "youtube.com/paulolopes"
+            },
+            { 
+                id: "SDIKHFBSDFNSDUIFHJ",
+                name: "Eu Machuco",
+                artist: "Faustao",
+                url: "youtube.com/paulolopes"
+             }
+        ]
+    }
+]
--- modulo4/intro-express/src/exe1.ts
+++ modulo4/intro-express/src/exe1.ts
@@ -1,15 +1,23 @@
-import express, { Response } from "express"
+import express from "express"
+import { users } from "./data1"
 import cors from "cors"
 
 const app = express()
 
 app.use (express.json())
+
 app.use(cors())
 
 app.listen(3003, () => {
-    console.log("Server is running in http//localhost:3003")
+    console.log("O backend está rodando na porta 3003")
 })
 
-app.get ("/", (request, response) => {
-    response.send("Hello World")
+app.get("/playlists", (req, res) => {
+    const allUsers = users
+
+    const allPlaylists = users.filter ((user) => {
+        user.tracks
+    })
+
+    res.send(allPlaylists)
 })
